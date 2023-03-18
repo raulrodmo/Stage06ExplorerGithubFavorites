b1a1ee40921f6a07f308e16754e8cab8ef52b273
raulrodmo <raulrm.dev@gmail.com>
2023-03-18 19:25:22 -0300

    connect to github api and add page icon

--- js/favorites.js
+++ js/favorites.js
@@ -1,3 +1,5 @@
+import { GithubUser } from "./GithubUser.js"
+
 export class Favorites {
   constructor(root){
     this.root = document.querySelector(root)
@@ -8,11 +10,40 @@ export class Favorites {
     this.entries = JSON.parse(localStorage.getItem('@github-favorites')) || []
   }
 
+  save(){
+    localStorage.setItem('@github-favorites', JSON.stringify(this.entries))
+  }
+
+  async add(username){
+    try {
+
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      if(userExists){
+        throw new Error('Usuário já cadastrado')
+      }
+
+      const user = await GithubUser.search(username)
+
+      if(user.login === undefined) {
+        throw new Error('Usuário não encontrado')
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+
+    } catch(error) {
+      alert(error.message)
+    }
+  }
+
   delete(user) {
     const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
 
     this.entries = filteredEntries
     this.update()
+    this.save()
   }
 }
 
@@ -23,6 +54,17 @@ export class FavoritesView extends Favorites{
     this.tbody = this.root.querySelector('table tbody')
 
     this.update()
+
+    this.onAdd()
+  }
+
+  onAdd(){
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    }
   }
 
   update(){
@@ -33,6 +75,7 @@ export class FavoritesView extends Favorites{
 
       row.querySelector('.user img').src= `https://github.com/${user.login}.png`
       row.querySelector('.user img').alt= `Imagem de${user.name}`
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
       row.querySelector('.user p').textContent = user.name
       row.querySelector('.user span').textContent = user.login
       row.querySelector('.repositories').textContent = user.public_repos
